8591c4b11a3b84544377cd599ad6d9651177f27f
moumita2022 <moumitachakr@googlemail.com>
2022-09-07 18:29:33 +0200

    BodyImage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+
+
 function App() {
     return (
         <div className="App">
             {/* <h1>The Foodie Express</h1> */}
             <Navbar />
+            <div className="container main">
+
+<HeroSection />
+            </div>
         </div>
     );
 }
+
 export default App;
