4a56c5f9609987f3e3f184391173341bcd7130f3
Hammed Waheed <worldwid01@gmail.com>
2022-09-08 00:52:10 +0200

    in progress

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 
-
 export default function App() {
-    return (
-        <div className="App">
-            {/* <h1>The Foodie Express</h1> */}
-            <Navbar />
-            <div className="container main">
-
-<HeroSection />
-            </div>
-        </div>
-    );
+	return (
+		<div className="App">
+			{/* <h1>The Foodie Express</h1> */}
+			<Navbar />
+			<div className="container main">
+				<HeroSection />
+			</div>
+		</div>
+	);
 }
-
-	
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import CustomImage from './CustomImage';
+import React from "react";
+import CustomImage from "./CustomImage";
 
 export default function HeroSection() {
+<<<<<<< HEAD
   const images = [
     "/img/gallery/1.jpg",
     "/img/gallery/2.jpg",
@@ -23,6 +24,36 @@ export default function HeroSection() {
       <div className='col gallery'>
         {images.map((src,index) => (<CustomImage key={index}imgSrc={src} pt={'90%'} />))}
 {/*<CustomImage pt={"85%"} />
+=======
+	const images = [
+		"/img/gallery/1.jpg",
+		"/img/gallery/2.jpg",
+		"/img/gallery/3.jpg",
+		"/img/gallery/4.jpg",
+		"/img/gallery/5.jpg",
+		"/img/gallery/6.jpg",
+		"/img/gallery/7.jpg",
+		"/img/gallery/8.jpg",
+		"/img/gallery/9.jpg",
+		"/img/gallery/10.jpg",
+	];
+	return (
+		<div className="section hero">
+			<div className="col typography">
+				<h1 className="title">What Are We About</h1>
+				<p className="info">
+					FoodieHub is a place where you can please your soul and tummy with
+					delicious food recepies of all cuisine.And our service is absolutely
+					free.So start exploring now.
+				</p>
+				<button className="btn">explore now</button>
+			</div>
+			<div className="col gallery">
+				{images.map((src, index) => (
+					<CustomImage key={index} imgSrc={src} pt={"90%"} />
+				))}
+				{/*<CustomImage pt={"85%"} />
+>>>>>>> 5400700 (in progress)
  <CustomImage pt={"85%"} />
 <CustomImage pt={"85%"} />
 <CustomImage pt={"85%"} />
@@ -31,7 +62,7 @@ export default function HeroSection() {
 <CustomImage pt={"85%"} />
 <CustomImage pt={"85%"} />
 <CustomImage pt={"85%"} /> */}
-    </div>
-    </div>
-  )
+			</div>
+		</div>
+	);
 }
