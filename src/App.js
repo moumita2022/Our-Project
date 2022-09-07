5c5bccb3313f0af8d52c2e5808f72bccf7086841
Hammed Waheed <worldwid01@gmail.com>
2022-09-07 11:34:13 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 function App() {
-    return (
-        <div className="App">
-            {/* <h1>The Foodie Express</h1> */}
-            <Navbar />
-        </div>
-    );
+	return (
+		<div className="App">
+			{/* <h1>The Foodie Express</h1> */}
+			<h1>Header</h1>
+			<Navbar />
+		</div>
+	);
 }
 export default App;
