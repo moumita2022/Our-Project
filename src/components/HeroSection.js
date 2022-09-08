911e24502c89669c0c973e1bf06fcef2620b5f7d
Hammed Waheed <worldwid01@gmail.com>
2022-09-08 09:59:44 +0200

    in progress

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import CustomImage from "./CustomImage";
 
 export default function HeroSection() {
+<<<<<<< HEAD
 const images = [
     "/img/gallery/1.jpg",
     "/img/gallery/2.jpg",
@@ -23,6 +24,58 @@ const images = [
       <div className='col gallery'>
         {images.map((src,index) => (<CustomImage key={index}imgSrc={src} pt={'90%'} />))}
 	  </div>	
+=======
+	const images = [
+		"/img/gallery/1.jpg",
+		"/img/gallery/2.jpg",
+		"/img/gallery/3.jpg",
+		"/img/gallery/4.jpg",
+		"/img/gallery/5.jpg",
+		"/img/gallery/6.jpg",
+		"/img/gallery/7.jpg",
+		"/img/gallery/8.jpg",
+		"/img/gallery/9.jpg",
+	];
+	return (
+		<div className="section hero">
+			<div className="col typography">
+				<h1 className="title">What Are We About</h1>
+				<p className="info">
+					FoodieHub is a place where you can please your soul and tummy with
+					delicious food recepies of all cuisine.And our service is absolutely
+					free.So start exploring now.
+				</p>
+				<button className="btn">explore now</button>
+			</div>
+			<div className="col gallery">
+				{images.map((src, index) => (
+					<CustomImage key={index} imgSrc={src} pt={"90%"} />
+				))}
+				{/*<CustomImage pt={"85%"} />
+
+	const images = [
+		"/img/gallery/1.jpg",
+		"/img/gallery/2.jpg",
+		"/img/gallery/3.jpg",
+		"/img/gallery/4.jpg",
+		"/img/gallery/5.jpg",
+		"/img/gallery/6.jpg",
+		"/img/gallery/7.jpg",
+		"/img/gallery/8.jpg",
+		"/img/gallery/9.jpg",
+		"/img/gallery/10.jpg",
+	];
+	return (
+		<div className="section hero">
+			<div className="col typography">
+				<h1 className="title">What Are We About</h1>
+				<p className="info">
+					FoodieHub is a place where you can please your soul and tummy with
+					delicious food recepies of all cuisine.And our service is absolutely
+					free.So start exploring now.
+				</p>
+				<button className="btn">explore now</button>
+>>>>>>> 74427f0 (in progress)
 			</div>
 	);
 }
